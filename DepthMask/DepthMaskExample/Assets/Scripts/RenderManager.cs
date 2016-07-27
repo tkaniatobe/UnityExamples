9722067e79e5f5a037616703d69b797c5348b6ad
Tali Kaniatobe <tkaniatobe@gmail.com>
2016-07-26 17:38:30 -0700

    Adding depth mask scene

---
+++ DepthMask/DepthMaskExample/Assets/Scripts/RenderManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderManager : MonoBehaviour {
+
+	void Start(){	
+		for (int i = 0; i < gameObject.GetComponent<Renderer>().materials.Length; i++) {
+			gameObject.GetComponent<Renderer>().materials[i].renderQueue = 2002;
+		}
+	}
+}
